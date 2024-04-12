991358aec3401d7be8a9d25b380cc6a9a5fc5876
chisom iwunze <iwunzekelvin98@gmail.com>
2024-04-11 21:58:40 -0700

    I simplify cahnges by using month- mothn view to see expenses

--- app/src/main/java/com/example/projectexpensetracker/MainActivity.kt
+++ app/src/main/java/com/example/projectexpensetracker/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.projectexpensetracker
 
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var monthSpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+       monthSpinner = findViewById<Spinner>(R.id.monthSpinner)
+        val months = arrayOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, months)
+        monthSpinner.adapter = adapter
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
